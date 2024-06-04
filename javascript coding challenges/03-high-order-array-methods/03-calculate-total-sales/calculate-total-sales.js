14b8080d5bb9341d5e9304973fba0f103057af1f
Nilesh Vincent <nileshvincent@incubatelabs.com>
2024-06-04 16:59:29 +0530

    jun 4 v1

--- javascript coding challenges/03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ javascript coding challenges/03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,12 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxPrecentage) {
+  const totalSales = products.reduce(
+    (total, value) => total + value.price * value.quantity,
+    0
+  );
+
+  const withTax = totalSales + (totalSales * taxPrecentage) / 100;
+
+  return withTax;
+}
 
 module.exports = calculateTotalSalesWithTax;
